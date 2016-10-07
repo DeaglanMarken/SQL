37715cb7f63bf8dc52d9f7d6c76fa74aa6b0f98d
Deaglan Marken <deaglanm@kainos.com>
2016-10-07 14:11:32 +0100

    Original file

---
+++ testscript.sql
@@ -0,0 +1,9 @@
+/*
+* Test script for MySQL course
+*/
+
+CREATE DATABASE test_from_script;
+USE test_from_script
+CREATE TABLE test_table (i int, v VARCHAR(20));
+INSERT INTO test_table VALUES (1, "hello") (2, "world");
+
