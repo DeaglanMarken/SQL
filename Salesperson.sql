bb335057a200704b4e27fbd2ce30fde59da505c9
Deaglan Marken <deaglanm@kainos.com>
2016-10-09 22:03:35 +0100

    Creates Salesperson table

---
+++ Salesperson.sql
@@ -0,0 +1,11 @@
+/*
+* Creates a table called salesperson
+*/
+CREATE TABLE Salesperson (
+Salesperson_id int PRIMARY KEY AUTO_INCREMENT,
+Salesperson_Name VARCHAR(30),
+Start_Date date,
+Contracted_Weekly_Hours decimal,
+Salary_£ decimal,
+FT boolean
+);
