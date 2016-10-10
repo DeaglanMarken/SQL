8e290c5c4452d09388910cd11b768e75065af27f
Deaglan Marken <deaglanm@kainos.com>
2016-10-10 09:24:56 +0100

    Original product fie creates product table

---
+++ product.sql
@@ -0,0 +1,8 @@
+/*
+* Creates a product table for the  Customer Database
+*/
+CREATE TABLE Product(
+Product_ID int PRIMARY KEY AUTO_INCREMENT,
+Product_Make VARCHAR(30),
+Product_Model VARCHAR(30)
+);
