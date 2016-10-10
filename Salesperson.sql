643a8765420bf452d372fcef00e750682dc110cc
Deaglan Marken <deaglanm@kainos.com>
2016-10-10 10:21:52 +0100

    Creates relationship between store and Salesperson

--- Salesperson.sql
+++ Salesperson.sql
@@ -4,8 +4,10 @@
 CREATE TABLE Salesperson (
 Salesperson_id int PRIMARY KEY AUTO_INCREMENT,
 Salesperson_Name VARCHAR(30),
+ID int (11) NOT NULL,
 Start_Date date,
 Contracted_Weekly_Hours decimal,
 Salary_£ decimal,
-FT boolean
+FT boolean,
+FOREIGN KEY (ID) REFERENCES Store(ID)
 );
